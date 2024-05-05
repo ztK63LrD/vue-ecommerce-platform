ee411eae2489298ebf4997173a7a2b16f25e5af0
亦世凡华 <2014106247@qq.com>
2024-05-05 16:16:32 +0800

    初始化项目
    对项目进行初始化操作，给出基础代码

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+// vite.config.ts
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, 'src') // 相对路径别名配置，使用 @ 代替 src
+        }
+    }
+})

f897d743955bc5a84db371f1a1a96d67369d151d
亦世凡华 <2014106247@qq.com>
2024-06-23 19:01:35 +0800

    同步

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-    <global-menu></global-menu>
-    <global-header></global-header>
-    <router-view></router-view>
-    <global-footer></global-footer>
+    <template v-if="route.path != '/login'">
+        <global-menu></global-menu>
+        <global-header></global-header>
+        <router-view></router-view>
+        <global-footer></global-footer>
+    </template>
+    <template v-else>
+        <router-view></router-view>
+    </template>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+let route = useRoute()
 
 </script>
 
